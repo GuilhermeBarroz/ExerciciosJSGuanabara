1ee204a15a7411ec8c5db2aac61d39b1521a0b53
unknown <guilhermebarroz@icloud.com>
2024-10-09 13:51:43 -0300

    Exercico 007 finalizado

--- nivel1/ex007/script.js
+++ nivel1/ex007/script.js
@@ -7,6 +7,7 @@ function calcularMedia(){
     let botao = document.getElementById("botao");
     
     botao.addEventListener("click", (e) => {
+        let span = document.getElementById("span").innerHTML
         let nome = prompt("Qual é o nome do aluno?");
         let nota1 = Number(prompt(`Qual a primeira nota de ${nome}?`));
         let nota2 = Number(prompt(`Além de ${nota1} qual a segunda nota de ${nome}?`));
@@ -15,6 +16,9 @@ function calcularMedia(){
 
         let media = soma / 2
         
-        console.log(nota1, nota2, soma, media)
+        document.getElementById("span").innerHTML = (`Calculando a média final de ${nome}<br>
+            As notas obtidas foram ${nota1} e ${nota2}<br>
+            A média final é ${media}`
+        )
     })
 }
